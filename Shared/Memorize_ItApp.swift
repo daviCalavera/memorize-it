88beb528bd2b172710375a3cec686c83d353852a
David Luna <dluna@toolboxtve.com>
2021-03-01 16:34:42 -0300

    Initial Commit

---
+++ Shared/Memorize_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_ItApp.swift
+//  Shared
+//
+//  Created by David on 01/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
